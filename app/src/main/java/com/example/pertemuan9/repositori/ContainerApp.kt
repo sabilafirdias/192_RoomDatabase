e03e70c593e3681434394ba2e8201955c7d03d32
sabilafirdias <sabilafirdias@gmail.com>
2025-11-28 15:40:06 +0700

    membuat interface containerapp

---
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan9.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.pertemuan9.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
