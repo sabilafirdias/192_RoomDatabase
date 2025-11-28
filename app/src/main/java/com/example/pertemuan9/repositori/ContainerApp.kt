dae1c974f743f8addbbd9e5b878b5c35672f22a8
sabilafirdias <sabilafirdias@gmail.com>
2025-11-28 15:40:38 +0700

    membuat class container data app dan aplikasi siswa di containerapp.kt

--- app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -8,3 +8,23 @@ interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+        override val repositoriSiswa: RepositoriSiswa by lazy {
+            OfflineRepositoriSiswa(
+                DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+    }
+
+class AplikasiSiswa : Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
