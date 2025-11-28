2057474aba29205829ad4e5f83ef318ccc393057
sabilafirdias <sabilafirdias@gmail.com>
2025-11-28 15:43:23 +0700

    membuat object destinasihome

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan9.view.route
+
+import com.example.pertemuan9.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+            override val titleRes = R.string.app_name
+}
