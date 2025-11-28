46a247ecf79a65b005c37e11dc6323149144332c
sabilafirdias <sabilafirdias@gmail.com>
2025-11-28 15:43:41 +0700

    membuat interface destinasinavigasi

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan9.view.route
+
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman
+     */
+
+    val titleRes: Int
+}
